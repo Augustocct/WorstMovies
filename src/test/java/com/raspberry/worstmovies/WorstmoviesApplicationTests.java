1fea6c1c23e3aa384dd1ffad93f4fbed36de814f
Augustocct <augustoconte2710@gmail.com>
2025-05-06 22:04:03 -0300

    Ajuste no teste

--- src/test/java/com/raspberry/worstmovies/WorstmoviesApplicationTests.java
+++ src/test/java/com/raspberry/worstmovies/WorstmoviesApplicationTests.java
@@ -17,19 +17,29 @@ class WorstmoviesApplicationTests {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldReturnAwardIntervals() throws Exception {
-        mockMvc.perform(get("/api/awards/intervals"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.min").isArray())
-                .andExpect(jsonPath("$.max").isArray())
-                .andExpect(jsonPath("$.min[0].producer").isNotEmpty())
-                .andExpect(jsonPath("$.min[0].interval").isNumber())
-                .andExpect(jsonPath("$.min[0].previousWin").isNumber())
-                .andExpect(jsonPath("$.min[0].followingWin").isNumber())
-                .andExpect(jsonPath("$.max[0].producer").isNotEmpty())
-                .andExpect(jsonPath("$.max[0].interval").isNumber())
-                .andExpect(jsonPath("$.max[0].previousWin").isNumber())
-                .andExpect(jsonPath("$.max[0].followingWin").isNumber());
-    }
+public void testAwardIntervalsResponse_shouldMatchExpectedResult() throws Exception {
+    mockMvc.perform(get("/api/awards/intervals"))
+            .andExpect(status().isOk())
+            .andExpect(content().json("""
+                {
+                  "min": [
+                    {
+                      "producer": "Joel Silver",
+                      "interval": 1,
+                      "previousWin": 1990,
+                      "followingWin": 1991
+                    }
+                  ],
+                  "max": [
+                    {
+                      "producer": "Matthew Vaughn",
+                      "interval": 13,
+                      "previousWin": 2002,
+                      "followingWin": 2015
+                    }
+                  ]
+                }
+            """));
+}
 
 }
